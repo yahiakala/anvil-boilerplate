d5a95d9b739cbc2437545fe84fe49cfd9919511f
yahiakala <ymzkala@gmail.com>
2024-02-26 10:57:16 -0500

    added settings form

---
+++ client_code/Settings/__init__.py
@@ -0,0 +1,49 @@
+from ._anvil_designer import SettingsTemplate
+import anvil
+import anvil.users
+from anvil_extras import routing
+
+from .. import Global
+
+
+@routing.route('settings')
+class Settings(SettingsTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+        self.admin_data = {}
+        self.admin_data['sites'] = ['url 1', 'url 2']
+        self.link_portal.url = Global.customer_portal
+        if self.link_portal.url:
+            self.link_portal.visible = True
+        # self.rp_websites.items = self.admin_data['sites']
+
+    def btn_chg_pw_click(self, **event_args):
+        self.lbl_pw_error.visible = False
+        user = Global.user
+        if self.tb_oldpw.text and self.tb_newpw.text:
+            print('Changing password')
+            try:
+                anvil.users.reset_password(self.tb_oldpw.text, self.tb_newpw.text)
+                self.lbl_pw_error.text = 'Password Changed!'
+                self.lbl_pw_error.visible = True
+                self.tb_oldpw.text = ''
+                self.tb_newpw.text = ''
+            except anvil.users.AuthenticationFailed:
+                print('Auth failed')
+                self.lbl_pw_error.text = 'Old password is incorrect.'
+                self.lbl_pw_error.visible = True
+            except anvil.users.PasswordNotAcceptable:
+                self.lbl_pw_error.text = 'Passwords must be 8 characters or more.'
+                self.lbl_pw_error.visible = True
+        else:
+            self.lbl_pw_error.text = 'Old or new entered password is blank.'
+            self.lbl_pw_error.visible = True
+        
+
+    def btn_crawlsite_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        # TODO: validate URL first
+        # show some kind of loading message
+        # 'this may take a few minutes'
+        task = anvil.server.call('crawl_site', self.tb_site_input.text)
