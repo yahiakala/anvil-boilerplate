21fed7873d750ccc30a51974a10ef5d48b9f8cc2
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-03-04 03:01:23 +0000

    Created Signin and Signup; Edited settings; Deleted Login

---
+++ client_code/Signin/__init__.py
@@ -0,0 +1,36 @@
+from ._anvil_designer import SigninTemplate
+from anvil import *
+import anvil.users
+from anvil_extras import routing
+
+from .. import Global
+
+
+@routing.route('signin')
+class Signin(SigninTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+    def btn_google_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        user = anvil.users.login_with_google(remember=True)
+        if user:
+            Global.user = user
+            routing.set_url_hash('homedetail')
+
+    def btn_signin_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        user = anvil.users.login_with_email(self.tb_email.text, self.tb_password.text)
+        if user:
+            Global.user = user
+            routing.set_url_hash('homedetail')
+
+    def link_forgot_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        anvil.users.send_password_reset_email(self.tb_email.text)
+        alert(f'please check your email ({self.tb_email.text}) for a password reset link.')
+
+    def link_signup_click(self, **event_args):
+        """This method is called when the link is clicked"""
+        routing.set_url_hash('signup')
---
+++ client_code/Signup/__init__.py
@@ -0,0 +1,43 @@
+from ._anvil_designer import SignupTemplate
+from anvil import *
+import anvil.users
+from anvil_extras import routing
+
+from .. import Global
+
+
+@routing.route('signup')
+class Signup(SignupTemplate):
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+
+    def btn_google_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        user = anvil.users.signup_with_google(remember=True)
+        # TODO: catch UserExists exception
+        if user:
+            Global.user = user
+            routing.set_url_hash('homedetail')
+
+    def btn_signup_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        # TODO: check for password strength
+        proceed = self.tb_password_repeat_lost_focus()
+        if proceed and self.tb_password.text == self.tb_password_repeat.text:
+            user = anvil.users.signup_with_email(
+                self.tb_email.text, self.tb_password.text, remember=True
+            )
+            if user:
+                Global.user = user
+                routing.set_url_hash('homedetail')
+
+    def tb_password_repeat_lost_focus(self, **event_args):
+        """This method is called when the TextBox loses focus"""
+        if self.tb_password_repeat.text != '' and self.tb_password_repeat.text != self.tb_password.text:
+            self.lbl_password_match.visible = True
+            return False
+        else:
+            self.lbl_password_match.visible = False
+        # TODO: password strength checker
+        return True
