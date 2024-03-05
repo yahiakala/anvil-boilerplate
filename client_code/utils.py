ce5424c952b8e6a9eee4a2dfb824bff3cd536836
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-03-05 19:30:29 +0000

    Edited utils

--- client_code/utils.py
+++ client_code/utils.py
@@ -23,3 +23,11 @@ def print_timestamp(input_str):
     # current_time = dt.datetime.now(eastern_tz)
     # formatted_time = current_time.strftime('%H:%M:%S.%f')
     print(input_str, " : ", dt.datetime.now().strftime("%H:%M:%S.%f"))
+
+
+def login_with_form():
+    pass
+
+
+def signup_with_form():
+    pass
