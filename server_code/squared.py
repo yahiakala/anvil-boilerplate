ca5f5210537cf51bf9a43368c32f9236c6f10a94
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-03-14 15:55:50 +0000

    Edited squared

--- server_code/squared.py
+++ server_code/squared.py
@@ -21,31 +21,10 @@ def signup_with_email_custom(email, password, app_name):
 @anvil.server.callable(require_user=True)
 def add_mfa_method(password, mfa_method):
     """Add an mfa method."""
-    # import bcrypt
-    # user = anvil.users.get_user(allow_remembered=True)
-    # if bcrypt.checkpw(password.encode('utf-8'), user['password_hash'].encode('utf-8')):
-    #     if user['mfa']:
-    #         check_mfa = [i for i in user['mfa'] if i['id'] == mfa_method['id']]
-    #         if len(check_mfa) == 0:
-    #             user['mfa'] = user['mfa'] + [mfa_method]
-    #     else:
-    #         user['mfa'] = [mfa_method]
-    # else:
-    #     raise anvil.users.AuthenticationFailed('Incorrect password')
-    # return user
-    return utils.add_mfa_method(password, mfa_method)
+    return auth.add_mfa_method_squared(password, mfa_method)
 
 
 @anvil.server.callable(require_user=True)
 def delete_mfa_method(password, id):
     """Delete mfa method if the password is correct."""
-    # import bcrypt
-    # user = anvil.users.get_user(allow_remembered=True)
-    # if bcrypt.checkpw(password.encode('utf-8'), user['password_hash'].encode('utf-8')):
-    #     user['mfa'] = [i for i in user['mfa'] if i['id'] != id]
-    #     if len(user['mfa']) == 0:
-    #         user['mfa'] = None
-    # else:
-    #     raise anvil.users.AuthenticationFailed('Incorrect password.')
-    # return user
-    return utils.delete_mfa_method(password, id)
+    return auth.delete_mfa_method_squared(password, id)
