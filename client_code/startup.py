1e42c812ad5a3998f530f70ecb86f848c94afbbd
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-03-14 15:35:24 +0000

    Edited startup

--- client_code/startup.py
+++ client_code/startup.py
@@ -6,6 +6,10 @@ from .Router import Router
 from .BlankTemplate import BlankTemplate
 from .Static import Static
 
+@routing.redirect(path="app", priority=20, condition=lambda: Globals.user is None)
+def redirect_no_user():
+    return "sign"
+
 hash, pattern, dict = routing.get_url_components()
 
 routing.set_url_hash(hash)
