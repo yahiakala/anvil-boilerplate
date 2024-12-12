41db43743db39f44c7c64a1e5b01f5fec8cd81a1
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-12-12 20:57:17 +0000

    Edited Templates.Static, Templates.Router, settings and Templates.Website; Deleted Templates.BlankTemplate

--- client_code/Templates/BlankTemplate/__init__.py
+++
@@ -1,9 +0,0 @@
-from anvil import *
-
-from ._anvil_designer import BlankTemplateTemplate
-
-
-class BlankTemplate(BlankTemplateTemplate):
-    def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
-        self.init_components(**properties)
