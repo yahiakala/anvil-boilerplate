aa6f883ee305c998a4a0594905e3c9bbd30b3a16
yahiakalabs@gmail.com <yahiakalabs@gmail.com>
2024-12-14 20:32:22 +0000

    Created Components.Misc, Components.GoogleButton and Components; Deleted Misc

--- client_code/Components/Misc/__init__.py
+++ client_code/Components/Misc/__init__.py
@@ -1,8 +1,6 @@
 from anvil import *
 
 from ._anvil_designer import MiscTemplate
-
-
 class Misc(MiscTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
